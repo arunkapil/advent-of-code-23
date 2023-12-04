b3b52453eb4570e144846be5bae09eac33cfcab8
Arun U. Kapil <arunkapil@miamioh.edu>
2023-12-04 03:10:42 -0500

    Completed Day 3, Part 2

---
+++ day_03/part_2.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$input = file_get_contents(__DIR__ . '/input.txt');
+$lines = explode("\n", $input);
+
+$number_series = [];
+$symbol_series = [];
+foreach ($lines as $line_number => $line) {
+    $number_relations = [];
+    $symbol_relations = [];
+
+    // Every line must terminate in a dot
+    $line .= '.';
+
+    // Clear the trash
+    $entries = str_split($line);
+
+    // Find related numbers per line and save them
+    $cache = [];
+    foreach ($entries as $position => $char) {
+        if (is_numeric($char)) {
+            $cache[$position] = $char;
+        } else {
+            foreach ($cache as $i => $n) {
+                $number_relations[$i] = implode('', $cache);
+            }
+            $cache = []; // reset cache
+
+            // save valid symbols
+            if ($char == '*') {
+                $symbol_relations[$position] = $char;
+            }
+        }
+    }
+
+    $number_series[$line_number] = $number_relations;
+    $symbol_series[$line_number] = $symbol_relations;
+}
+
+// Remove empty symbol pages
+$symbol_series = array_filter($symbol_series);
+
+$result = [];
+foreach ($symbol_series as $page_number => $content) {
+
+    foreach ($content as $position => $symbol) {
+        $previous = $current = $next = [];
+
+        // Previous Page
+        if (array_key_exists($page_number - 1, $number_series)) {
+            $previous = searchPageForMatch(
+                $number_series[$page_number - 1],
+                [
+                    $position - 1,
+                    $position,
+                    $position + 1,
+                ]
+            );
+        }
+
+        // Current Page
+        $current = searchPageForMatch(
+            $number_series[$page_number],
+            [
+                $position - 1,
+                $position + 1,
+            ]
+        );
+
+        // // Previous Page
+        if (array_key_exists($page_number + 1, $number_series)) {
+            $next = searchPageForMatch(
+                $number_series[$page_number + 1],
+                [
+                    $position - 1,
+                    $position,
+                    $position + 1,
+                ]
+            );
+        }
+
+        $cleaned = array_merge(...array_values(array_filter([$previous, $current, $next])));
+
+        if (count($cleaned) > 1) {
+            $result["{$page_number}.{$position}"] = array_product($cleaned);
+        }
+    }
+}
+
+dd(array_sum($result));
+
+function searchPageForMatch(array $series, array $positions): array {
+    $values = [];
+    foreach ($positions as $position) {
+        if (array_key_exists($position, $series)) {
+            $value = $series[$position];
+            $values[$value] = true;
+        }
+    }
+
+    return array_keys($values);
+}
+
+
+
